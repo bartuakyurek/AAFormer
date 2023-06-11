70bf29e5be89cd342bc9b764b2b6d6b00b8b3bf6
bartuakyurek <77360680+bartuakyurek@users.noreply.github.com>
2023-06-11 12:32:21 +0300

    Add files via upload

---
+++ model/tokens.py
@@ -0,0 +1,14 @@
+"""
+
+This file is created under the CENG 502 Project.
+
+
+"""
+
+
+# Extracts the background pixels of a given image and its mask.
+# To be utilized in Algorithm 1 of the Supplementary Material.
+def _get_background():
+
+    pass
+
